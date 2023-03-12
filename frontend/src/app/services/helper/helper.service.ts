e904eb629aae1aee0f65dcff45244c5b55147217
Ankit Meshram <ankitdm69@gmail.com>
2023-03-12 18:54:13 +0530

    F18 - Created helper service for navigation and sidebar menu

---
+++ frontend/src/app/services/helper/helper.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  sideBarMenu: any = [];
+
+  loadSideBarMenu = (menu:any = null) => {
+    if (this.router.url.includes("dashboard")) {
+      return this.sideBarMenu = [
+        {
+          link: "./dashboard",
+          text: "Dashboard"
+        },
+        {
+          link: "./dashboard/courses",
+          text: "Courses"
+        },
+        {
+          link: "./dashboard",
+          text: "Orders"
+        },
+        {
+          link: "./dashboard",
+          text: "Profile"
+        },
+      ]
+    } else if (this.router.url.includes("admin")) {
+      return this.sideBarMenu = [
+        {
+          link: "./admin",
+          text: "Dashboard"
+        },
+        {
+          link: "./admin/courses",
+          text: "Courses"
+        },
+        {
+          link: "./admin",
+          text: "Orders"
+        },
+        {
+          link: "./admin/users",
+          text: "Users"
+        },
+      ]
+    }
+
+    return
+  }
+
+  toNavigate = (url: any) => {
+    this.loadSideBarMenu();
+    this.router.navigate([`${url}`]);
+  }
+
+
+
+}
