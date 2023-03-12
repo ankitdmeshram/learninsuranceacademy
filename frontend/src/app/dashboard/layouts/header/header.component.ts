2f26876e5c9ee393ea3039c342116e5e610e8cfb
Ankit Meshram <ankitdm69@gmail.com>
2023-03-12 16:38:11 +0530

    F17 - Added admin button if admin logged in to panel

--- frontend/src/app/dashboard/layouts/header/header.component.ts
+++ frontend/src/app/dashboard/layouts/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserService } from 'src/app/services/users/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,33 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['../../dashboard.component.css', './header.component.css']
 })
 export class HeaderComponent {
+  usersData: any;
+  userEmail: any;
+
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private user: UserService
   ) {
 
+    this.auth.getUser().then((res: any) => {
+      this.userEmail = res?.multiFactor.user.email
+    })
+
+    this.user.Users()
+      .subscribe((res: any) => {
+        this.usersData = res;
+        this.usersData.map((res: any) => {
+          if (this.userEmail == res.email) {
+            this.usersData = res;
+            if(this.usersData.role == 100)
+            {
+              this.auth.signOut()
+            }
+          }
+        })
+      })
   }
+
 
   signOut = () => {
     this.auth.signOut()
