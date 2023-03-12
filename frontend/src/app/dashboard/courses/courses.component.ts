324484087eff18c43d3c49486df2c9559e0c7787
Ankit Meshram <ankitdm69@gmail.com>
2023-03-12 18:57:12 +0530

    F20 - Course section in user section much be loaded automatically

--- frontend/src/app/dashboard/courses/courses.component.ts
+++ frontend/src/app/dashboard/courses/courses.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { CourseService } from 'src/app/services/course.service';
+import { HelperService } from 'src/app/services/helper/helper.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['../dashboard.component.css','./courses.component.css']
+  styleUrls: ['../dashboard.component.css', './courses.component.css']
 })
 export class CoursesComponent {
 
+  constructor(
+    private spinner: NgxSpinnerService,
+    private course: CourseService,
+    private toastr: ToastrService,
+    private helper: HelperService
+  ) {
+    this.Courses();
+    helper.loadSideBarMenu()
+  }
+  lessons: any = [];
+  courses: any = [];
+
+  toNavigate = (url: string) => {
+    this.viewLessons()
+    this.helper.toNavigate(`./dashboard/lessons/${url}`)
+  }
+
+  Courses = () => {
+    this.spinner.show()
+    this.course.viewCourses()
+      .subscribe((res) => {
+        this.courses = res;
+        this.spinner.hide();
+      },
+        (err) => {
+          this.toastr.error("Course not found");
+          this.spinner.hide();
+        }
+      )
+  }
+
+  viewLessons = () => {
+    this.course.viewLessons()
+      .subscribe((res: any) => {
+        console.log(res)
+        res.filter((res:any) => {
+          let data = {
+            link: "",
+            text: res.lesson_name
+          }
+          this.lessons.push(data)
+        })
+      })
+  }
 }
